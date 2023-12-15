5ce2c984fc3260987921591a8e9ded93e695b813
git-jessryll-workspace <62193217+git-jessryll-workspace@users.noreply.github.com>
2023-12-15 19:26:01 +0800

    unit testing for login

--- tests/Feature/LoginApiTest.php
+++ tests/Feature/LoginApiTest.php
@@ -2,35 +2,50 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\Services\LoginApiService;
+use Illuminate\Http\Request;
 use Tests\TestCase;
 
 class LoginApiTest extends TestCase
 {
-    public function testUserLogin()
+    public function testLoginWithValidCredentials()
     {
-        // Create a user for testing (adjust the factory to match your User model)
         $user = \App\Models\User::factory()->create([
             'email' => 'test@example.com',
             'password' => bcrypt('password'), // Use the actual password set in the factory
         ]);
 
-        // Send a request to login
-        $response = $this->postJson('/api/login', [
-            'email' => $user->email,
+        $loginApiService = new LoginApiService();
+
+        $request = new Request([
+            'email' => 'test@example.com',
             'password' => 'password', // Use the actual password set in the factory
         ]);
 
-        // Assert the response structure and values
-        $response->assertJsonStructure([
-            'status',
-            'message',
-            'token',
-        ])->assertJson([
-            'status' => true,
-            'message' => 'User Logged In Successfully',
+        $response = $loginApiService->login($request);
+
+        $responseContent = json_decode($response->getContent(), true);
+
+        $this->assertTrue($responseContent['status']);
+        $this->assertEquals('User logged in successfully', $responseContent['message']);
+        $this->assertArrayHasKey('token', $responseContent['data']);
+    }
+
+    public function testLoginWithInvalidCredentials()
+    {
+        $loginApiService = new LoginApiService();
+
+        $request = new Request([
+            'email' => 'invalid@example.com',
+            'password' => 'invalidpassword',
         ]);
 
+        $response = $loginApiService->login($request);
+
+        $responseContent = json_decode($response->getContent(), true);
+
+        $this->assertFalse($responseContent['status']);
+        $this->assertEquals('Email & Password do not match our records.', $responseContent['message']);
+        $this->assertNull($responseContent['data']);
     }
 }
